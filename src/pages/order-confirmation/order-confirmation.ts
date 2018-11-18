da507f768bfe12991f6449a1e840d37dcb20ead2
leonard.gregorio <leonard.gregorio@gmail.com>
2018-11-18 17:23:18 -0200

    informando pedido registrado ao usuario

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -22,6 +22,9 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codpedido: string;
+
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -55,11 +58,16 @@ export class OrderConfirmationPage {
     this.navCtrl.setRoot('CartPage');
   }
 
+  home() {
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
+
   checkOut() {
     this.pedidoService.insert(this.pedido)
       .subscribe(response => {
-        this.cartService.createOrClearCart;
-        console.log(response.headers.get('location'));
+        this.cartService.createOrClearCart();
+        this.codpedido = this.extractId(response.headers.get('location'));
       },
         error => {
           if (error.status == 403) {
@@ -68,4 +76,8 @@ export class OrderConfirmationPage {
         });
   }
 
+  private extractId(location: string): string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
+  }
 }
